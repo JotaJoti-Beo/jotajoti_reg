34f4c08de409536f1b662908a9dc1fea16a5ad8f
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-04-18 16:26:23 +0200

    Update helpers

--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 7.6.2 on 2020-04-18 13:10:38.
+ * Generated for Laravel 7.6.2 on 2020-04-18 14:18:57.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
