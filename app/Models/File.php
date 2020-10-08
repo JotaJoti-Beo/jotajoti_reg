512542bdbbf31871eb027d39b4fdd8e8f32aca6f
Vento-Nuenenen <vento@pfadi-nuenenen.ch>
2020-10-08 21:21:26 +0200

    Add model

---
+++ app/Models/File.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class File extends Model
+{
+    protected $table = 'file';
+
+    protected $fillable = [
+        'file_name',
+        'participant_id'
+    ];
+
+    public function participant(){
+        return $this->hasOne('App\Models\Participant');
+    }
+}
