337da901e87b7b3a7172c2750ba7bab384c36a12
Vento <vento@pfadi-nuenenen.ch>
2020-12-20 15:51:26 +0100

    Add profile to nav

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -77,6 +77,10 @@
                         </a>
 
                         <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item {{ Request::is('admin/profile/')}}" href="{{ url('/admin/profile') }}">
+                                Profil
+                            </a>
+
                             <a class="dropdown-item" href="{{ route('logout') }}"
                                onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
