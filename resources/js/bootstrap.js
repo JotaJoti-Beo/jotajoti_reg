9fab5e900601403db491d82080095cb290e3f353
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-05-11 18:31:52 +0200

    Package upgrade

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,6 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-    window.Popper = require('popper.js').default;
 
     require('bootstrap');
 } catch (e) {}
