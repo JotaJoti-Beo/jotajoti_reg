fb573c583eb0f7aa0b2cbb34df41c2de29b709bd
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-09-26 01:03:19 +0200

    Update UsersSeeder.php

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -13,6 +13,25 @@ class UsersSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $seededAdminEmail = 'admin@jojo.ch';
+        $user = User::where('email', '=', $seededAdminEmail)->first();
+        if ($user === null) {
+            $user = User::create(['scout_name' => 'Admin', 'first_name' => 'Admin', 'last_name' => 'Admin', 'email' => $seededAdminEmail, 'password' => Hash::make('password')]);
+            $user->save();
+        }
+
+        // Seed test user
+        $seededUserEmail = 'caspar.brenneisen@protonmail.ch';
+        $user = User::where('email', '=', $seededUserEmail)->first();
+        if ($user === null) {
+            $user = User::create([
+                'scout_name'                     => 'Vento',
+                'first_name'                     => 'Caspar',
+                'last_name'                      => 'Brenneisen',
+                'email'                          => $seededUserEmail,
+                'password'                       => Hash::make('password'),
+            ]);
+            $user->save();
+        }
     }
 }
