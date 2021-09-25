c182bf504e1a58a53370f3c60ae6f77d57937ebc
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-09-25 19:16:23 +0200

    Update OverwatchController.php

--- app/Http/Controllers/Backend/OverwatchController.php
+++ app/Http/Controllers/Backend/OverwatchController.php
@@ -3,12 +3,11 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 
 class OverwatchController extends Controller
 {
     public function index()
     {
-        return view('backend.overwatch.overwatch');
+        return view('backend.overwatch.index');
     }
 }
