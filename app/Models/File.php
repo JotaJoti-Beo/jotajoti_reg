a4cbc174dbbb8c43d6eba41f5b047b41182da208
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-09-25 19:50:05 +0200

    Update File.php

--- app/Models/File.php
+++ app/Models/File.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
+    use HasFactory;
+
     protected $table = 'file';
 
     protected $fillable = [
         'file_name',
-        'participant_id',
+        'participant_id'
     ];
 
     public function participant()
