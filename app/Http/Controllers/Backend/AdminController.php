44b45e4635eb2d09ce2cffe8e84c8430b74545a7
Vento <vento@pfadi-nuenenen.ch>
2021-09-26 11:55:01 +0200

    Update AdminController.php

--- app/Http/Controllers/Backend/AdminController.php
+++ app/Http/Controllers/Backend/AdminController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Config;
 use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
     public function index()
     {
+        $admin = Config::all();
 
+        return view('backend.config.index', ['config' => $admin]);
     }
 }
