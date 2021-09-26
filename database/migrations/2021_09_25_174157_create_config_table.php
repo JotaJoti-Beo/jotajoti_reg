251294fae63248c8676708e3b943d900c5e1ae5d
Vento <vento@pfadi-nuenenen.ch>
2021-09-26 11:54:11 +0200

    Create 2021_09_25_174157_create_config_table.php

---
+++ database/migrations/2021_09_25_174157_create_config_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateConfigTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->id();
+            $table->date('reg_start')->nullable();
+            $table->date('jojo_start')->nullable();
+            $table->date('jojo_start_pio')->nullable();
+            $table->date('jojo_end')->nullable();
+            $table->integer('max_tn');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+}
