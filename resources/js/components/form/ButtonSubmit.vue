f27ab64998132942150c3e41c78c92a6f16d94e5
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-10-01 23:59:01 +0200

    Create ButtonSubmit.vue

---
+++ resources/js/components/form/ButtonSubmit.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="form-group row mb-0">
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ButtonSubmit",
+    props: {
+        label: { type: String, default: function () { return this.$t('t.global.save') } },
+    }
+}
+</script>
+
+<style scoped></style>
