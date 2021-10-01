6d0678880f5744b03bb958f19a8877e7296f1076
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-10-01 23:49:14 +0200

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,6 @@
+import Vue from "vue";
+import { kebabCase } from 'lodash';
+
 require('./bootstrap');
 
 $(document).ready(function () {
@@ -6,8 +9,8 @@ $(document).ready(function () {
 
 window.Vue = require('vue').default;
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
