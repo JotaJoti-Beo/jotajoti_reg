71ea96f8856dbd8a1aa8ca5fcd7c9108ae144a4a
Vento <vento@pfadi-nuenenen.ch>
2021-10-02 22:35:44 +0200

    Update edit.blade.php

--- resources/views/backend/groups/edit.blade.php
+++ resources/views/backend/groups/edit.blade.php
@@ -15,23 +15,42 @@
                 <a href="{{ route('groups') }}" class="float-right">Zurück zu Gruppen</a>
             </div>
             <div class="card-body">
-                {!! Form::open(array('route' => ['update-groups',$groups->id], 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
+                {!! Form::open(array('route' => ['update-groups',$group->id], 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
                 {!! csrf_field() !!}
 
-                <div class="form-group has-feedback row {{ $errors->has('group_name') ? ' has-error ' : '' }}">
-                    {!! Form::label('group_name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
+                <div class="form-group has-feedback row {{ $errors->has('name') ? ' has-error ' : '' }}">
+                    {!! Form::label('name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
                     <div class="col-md-9">
                         <div class="input-group">
-                            {!! Form::text('group_name', old('group_name', $groups->group_name ?? null), array('id' => 'group_name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
+                            {!! Form::text('name', old('name', $group->name ?? null), array('id' => 'name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
                             <div class="input-group-append">
-                                <label class="input-group-text" for="group_name">
+                                <label class="input-group-text" for="name">
                                     <i class="fa fa-group" aria-hidden="true"></i>
                                 </label>
                             </div>
                         </div>
-                        @if ($errors->has('group_name'))
+                        @if ($errors->has('name'))
                             <span class="help-block">
-                                <strong>{{ $errors->first('group_name') }}</strong>
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group has-feedback row {{ $errors->has('quota') ? ' has-error ' : '' }}">
+                    {!! Form::label('quota', 'Quota', array('class' => 'col-md-3 control-label')); !!}
+                    <div class="col-md-9">
+                        <div class="input-group">
+                            {!! Form::number('quota', old('quota', $group->quota ?? null), array('id' => 'quota', 'class' => 'form-control', 'placeholder' => 'Quota')) !!}
+                            <div class="input-group-append">
+                                <label class="input-group-text" for="quota">
+                                    <i class="fa fa-group" aria-hidden="true"></i>
+                                </label>
+                            </div>
+                        </div>
+                        @if ($errors->has('quota'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('quota') }}</strong>
                             </span>
                         @endif
                     </div>
