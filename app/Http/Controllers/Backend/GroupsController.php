5c8ea925c16aef55e5f1638ce29e700a402137e0
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-10-05 18:41:44 +0200

    Update GroupsController.php

--- app/Http/Controllers/Backend/GroupsController.php
+++ app/Http/Controllers/Backend/GroupsController.php
@@ -8,8 +8,6 @@ use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use DB;
 use App\Models\Group;
 
 class GroupsController extends Controller
