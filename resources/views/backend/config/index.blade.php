21eed65f8536b7fc8a46a84e866e47b7372534a0
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2021-10-05 08:51:48 +0200

    Update index.blade.php

--- resources/views/backend/config/index.blade.php
+++ resources/views/backend/config/index.blade.php
@@ -8,33 +8,56 @@
             </div>
         @endif
 
+            <div class="card mb-3">
+                <div class="card-header">
+                    <div class="input-group" id="adv-search">
+                        <button onclick="location.href='{{ route('config.update', 1) }}'" type="button" class="btn btn-primary form-control mt-2">Konfigurieren</button>
+                    </div>
+                </div>
+            </div>
+
         <div class="card">
             <div class="card-header">
-                <h5 class="float-left">Alle Gruppen</h5>
+                <h5 class="float-left">Aktuelle Konfiguration</h5>
 
                 <a href="{{  route('overwatch') }}" class="float-right">Zurück zu Overwatch</a>
             </div>
             <div class="card-body table-responsive">
                 <table class="table table-hover">
                     <thead>
-                    <th>
-                        Reg-Datum
-                    </th>
-                    <th>
-                        Optionen
-                    </th>
+                        <th>
+                            Freigabe Anmeldung
+                        </th>
+                        <th>
+                            JotaJoti Start
+                        </th>
+                        <th>
+                            JotaJoti Start Pios
+                        </th>
+                        <th>
+                            Maximum TN
+                        </th>
                     </thead>
                     <tbody>
-                    @foreach($config as $entry)
-                        <tr>
-                            <td>
-                                {{ $entry->reg_start }}
-                            </td>
-                            <td>
-                                <button onclick="location.href='{{ route('',$group->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-edit"></span></button>
-                            </td>
-                        </tr>
-                    @endforeach
+                        @foreach($config as $entry)
+                            <tr>
+                                <td>
+                                    {{ $entry->reg_start }}
+                                </td>
+                                <td>
+                                    {{ $entry->jojo_start }}
+                                </td>
+                                <td>
+                                    {{ $entry->jojo_start_pio }}
+                                </td>
+                                <td>
+                                    {{ $entry->jojo_end }}
+                                </td>
+                                <td>
+                                    {{ $entry->max_tn }}
+                                </td>
+                            </tr>
+                        @endforeach
                     </tbody>
                 </table>
             </div>
