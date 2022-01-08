779c1a975c5466e84626c07397a1bdfd6ac35716
Vento <vento@pfadi-nuenenen.ch>
2022-01-08 17:41:05 +0100

    Rename files table

--- database/migrations/2021_09_25_174242_create_files_table.php
+++ database/migrations/2021_09_25_174242_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateFileTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,9 @@ class CreateFileTable extends Migration
      */
     public function up()
     {
-        Schema::create('file', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->id();
-            $table->string('file_name');
+            $table->string('name');
             $table->foreignId('participant_id');
             $table->timestamps();
         });
