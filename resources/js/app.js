2ab3581e0b2b239d69dcbf28575fd0405b543f63
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-01-11 18:20:35 +0100

    Update app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,5 @@
-import Vue from "vue";
-
 require('./bootstrap');
 
-$(document).ready(function () {
-   $('select').selectpicker();
-});
-
-window.Vue = require('vue').default;
-
-Vue.component('register-form-component', require('./components/RegisterFormComponent.vue').default);
-
-const app = new Vue({
-    el: '#app',
+$(document).ready(function (){
+    $('select').selectpicker();
 });
