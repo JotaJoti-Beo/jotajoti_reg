83be5e9d9006d8cbbe1eac0485a24ac9931f37fd
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-20 18:37:55 +0100

    Update frontend.blade.php

--- resources/views/layouts/frontend.blade.php
+++ resources/views/layouts/frontend.blade.php
@@ -1,32 +1,9 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+@extends('layouts.app')
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}" />
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ mix('js/app.js') }}" defer></script>
-
-    <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com" />
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" />
-
-    <!-- Styles -->
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" />
-</head>
-<body>
-    <div class="container">
+@section('index-frame')
+    <div id="app">
         <main class="py-4">
             @yield('content')
         </main>
     </div>
-
-    @yield('script')
-</body>
-</html>
+@endsection
