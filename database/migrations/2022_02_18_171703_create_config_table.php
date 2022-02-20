ce9e0fbc942189c25fb44c679a61a8f7dd599036
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-20 16:31:08 +0100

    Create 2022_02_18_171703_create_config_table.php

---
+++ database/migrations/2022_02_18_171703_create_config_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('config', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('reg_start')->nullable();
+            $table->dateTime('we_start')->nullable();
+            $table->dateTime('we_start_pio');
+            $table->dateTime('we_end');
+            $table->integer('quota');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('config');
+    }
+};
