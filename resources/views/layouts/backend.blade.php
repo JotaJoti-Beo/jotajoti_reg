d214f663301f7e90307c6dc9a4e1f88ebc0a282b
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-20 18:37:59 +0100

    Update backend.blade.php

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -1,1 +1,11 @@
+@extends('layouts.app')
 
+@section('index-frame')
+    @include('layouts.nav')
+
+    <div id="app">
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+@endsection
