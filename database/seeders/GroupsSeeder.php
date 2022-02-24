820e7ce9085ade20518c59e9c5e9fc063166edd6
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-02-24 18:14:27 +0100

    Update GroupsSeeder.php

--- database/seeders/GroupsSeeder.php
+++ database/seeders/GroupsSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
 use App\Models\Group;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
 
 class GroupsSeeder extends Seeder
 {
