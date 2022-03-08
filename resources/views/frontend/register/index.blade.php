2157f1eb9c99f865874ecc1b00e55fa39d2bcb92
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-03-08 18:05:59 +0100

    Update index.blade.php

--- resources/views/frontend/register/index.blade.php
+++ resources/views/frontend/register/index.blade.php
@@ -1,94 +1,26 @@
 @extends('layouts.frontend')
 
 @section('content')
-    <div class="jumbotron">
-        <h1>Jota Joti Beo - Anmeldung {{ "2022" }}</h1>
-        <br />
+    <div class="container-fluid text-center">
+        <h1>Wilkommen bei der Anmeldung zum JotaJoti Beo {{ "2022" }}</h1>
+    </div>
 
-        <form method="post" action="/" accept-charset="UTF-8" role="form" class="needs-validation">
+    <br />
 
-        </form>
-        {!! Form::open(['route' => 'home', 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation']) !!}
+    <div class="container-fluid">
+        <h2 class="text-center">Teilnahmebedingungen:</h2>
 
-            <div class="form-group has-feedback row {{ $errors->has('name') ? ' has-error ' : '' }}">
-            {!! Form::label('name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
-            <div class="col-md-9">
-                <div class="input-group">
-                    {!! Form::text('name', NULL, array('id' => 'name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
-                    <div class="input-group-append">
-                        <label class="input-group-text" for="name">
-                            <i class="fa fa-group" aria-hidden="true"></i>
-                        </label>
-                    </div>
-                </div>
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                @endif
-            </div>
-        </div>
+        <p>
 
-        <div class="form-group has-feedback row {{ $errors->has('name') ? ' has-error ' : '' }}">
-            {!! Form::label('name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
-            <div class="col-md-9">
-                <div class="input-group">
-                    {!! Form::text('name', NULL, array('id' => 'name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
-                    <div class="input-group-append">
-                        <label class="input-group-text" for="name">
-                            <i class="fa fa-group" aria-hidden="true"></i>
-                        </label>
-                    </div>
-                </div>
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                @endif
-            </div>
-        </div>
-
-        <div class="form-group has-feedback row {{ $errors->has('name') ? ' has-error ' : '' }}">
-            {!! Form::label('name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
-            <div class="col-md-9">
-                <div class="input-group">
-                    {!! Form::text('name', NULL, array('id' => 'name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
-                    <div class="input-group-append">
-                        <label class="input-group-text" for="name">
-                            <i class="fa fa-group" aria-hidden="true"></i>
-                        </label>
-                    </div>
-                </div>
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                @endif
-            </div>
-        </div>
-
-        <div class="form-group has-feedback row {{ $errors->has('name') ? ' has-error ' : '' }}">
-            {!! Form::label('name', 'Gruppenname', array('class' => 'col-md-3 control-label')); !!}
-            <div class="col-md-9">
-                <div class="input-group">
-                    {!! Form::text('name', NULL, array('id' => 'name', 'class' => 'form-control', 'placeholder' => 'Gruppenname')) !!}
-                    <div class="input-group-append">
-                        <label class="input-group-text" for="name">
-                            <i class="fa fa-group" aria-hidden="true"></i>
-                        </label>
-                    </div>
-                </div>
-                @if ($errors->has('name'))
-                    <span class="help-block">
-                                <strong>{{ $errors->first('name') }}</strong>
-                            </span>
-                @endif
-            </div>
-        </div>
+        </p>
 
         <br />
 
-            {!! Form::button('Anmeldung senden', array('class' => 'btn btn-success col-12','type' => 'submit' )) !!}
-        {!! Form::close() !!}
+        <h2 class="text-center">Die neusten Infos:</h2>
+
+        <br />
+
+        <h2 class="text-center">Teilnehmen:</h2>
+        <button>Anmelde-Formular</button>
     </div>
 @endsection
