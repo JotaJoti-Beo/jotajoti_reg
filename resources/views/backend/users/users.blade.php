4fec290094e41726e4ad3e48248af850445236e6
Caspar Brenneisen <caspar.brenneisen@fadendaten.ch>
2022-03-08 18:40:24 +0100

    Create users.blade.php

---
+++ resources/views/backend/users/users.blade.php
@@ -0,0 +1,74 @@
+@extends('layouts.backend')
+
+@section('content')
+    <div class="col-12">
+        @if(session()->has('message'))
+            <div class="alert alert-success">
+                {{ session()->get('message') }}
+            </div>
+        @endif
+    </div>
+
+    <div class="card col-md-10 offset-md-1">
+        <div class="card-header">
+            {!! Form::open(array('route' => 'users', 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
+            <div class="input-group" id="adv-search">
+                {!! Form::text('search', NULL, array('id' => 'search', 'class' => 'form-control', 'placeholder' => 'Suche', 'autofocus')) !!}
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary form-control">
+                        <span class="fa fa-search"></span>
+                    </button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+            <div class="input-group" id="adv-search">
+                <button onclick="location.href='{{ route('add-users') }}'" type="button" class="btn btn-primary form-control mt-2">Neuer Benutzer</button>
+            </div>
+        </div>
+    </div>
+
+    <br />
+
+    <div class="card col-md-10 offset-md-1">
+        <div class="card-header">
+            <h5 class="float-start">Benutzer</h5>
+
+            <a href="{{  route('overwatch') }}" class="float-end">Zurück zu Overwatch</a>
+        </div>
+        <div class="card-body table-responsive">
+            <table class="table table-hover">
+                <thead>
+                <th>
+                    Name
+                </th>
+                <th>
+                    E-Mail
+                </th>
+                <th>
+                    Optionen
+                </th>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
+                    <tr>
+                        <td>
+                            @if($user->scout_name)
+                                {{ $user->first_name }} {{ $user->last_name }} / {{ $user->scout_name }}
+                            @else
+                                {{ $user->first_name }} {{ $user->last_name }}
+                            @endif
+                        </td>
+                        <td>
+                            {{ $user->email }}
+                        </td>
+                        <td>
+                            <button onclick="location.href='{{ route('edit-users', $user->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-edit"></span></button>
+                            <button onclick="location.href='{{ route('destroy-users', $user->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-remove"></span></button>
+                        </td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
