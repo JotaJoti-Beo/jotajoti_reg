8e4b9723ebe88550a516fe2aac3db8e301b9c019
Vento <vento@pfadi-nuenenen.ch>
2022-05-28 01:11:41 +0200

    Create index.blade.php

---
+++ resources/views/backend/guardians/index.blade.php
@@ -0,0 +1,76 @@
+@extends('layouts.backend')
+
+@section('content')
+	<div class="col-12">
+		@if(session()->has('message'))
+			<div class="alert alert-success">
+				{{ session()->get('message') }}
+			</div>
+		@endif
+    </div>
+
+    <div class="card col-md-10 offset-md-1">
+        <div class="card-header">
+            {!! Form::open(array('route' => 'guardians', 'method' => 'POST', 'role' => 'form', 'class' => 'needs-validation')) !!}
+            <div class="input-group" id="adv-search">
+                {!! Form::text('search', NULL, array('id' => 'search', 'class' => 'form-control', 'placeholder' => 'Suche')) !!}
+                <div class="input-group-append">
+                    <button type="submit" class="btn btn-primary form-control">
+                        <span class="fa fa-search"></span>
+                    </button>
+                </div>
+            </div>
+            {!! Form::close() !!}
+            <div class="input-group" id="adv-search">
+                <button onclick="location.href='{{ route('add-guardians') }}'" type="button" class="btn btn-primary form-control mt-2">Neue Erziehungsberechtigte</button>
+            </div>
+        </div>
+    </div>
+
+    <br />
+
+    <div class="card col-md-10 offset-md-1">
+        <div class="card-header">
+            <h5 class="float-start">Erziehungsberechtigte</h5>
+
+            <a href="{{ route('guardians') }}" class="float-end">Zurück zu Erziehungsberechtigte</a>
+        </div>
+        <div class="card-body table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <th>
+                        Name
+                    </th>
+                    <th>
+                        Abteilung
+                    </th>
+                    <th>
+                        Exer
+                    </th>
+                    <th>
+                        Optionen
+                    </th>
+                </thead>
+                <tbody>
+                    @foreach($guardians as $guardian)
+                        <tr>
+                            <td>
+                                {{ $guardian->first_name }} {{ $guardian->last_name }}
+                            </td>
+                            <td>
+                                {{ $guardian->group_name }}
+                            </td>
+                            <td>
+                                {{ $guardian->exer_name }}
+                            </td>
+                            <td>
+                                <button onclick="location.href='{{ route('edit-participations',$guardian->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-edit"></span></button>
+                                <button onclick="location.href='{{ route('destroy-participations',$guardian->id) }}'" class="btn btn-danger ml-2"><span class="fa fa-remove"></span></button>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
