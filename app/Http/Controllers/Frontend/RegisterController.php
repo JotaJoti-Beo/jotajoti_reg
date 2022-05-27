ebb4f04c9ed193b2933ccac7388f60e8663c9a3b
Caspar Brenneisen <github@brenneisen.me>
2022-05-27 17:41:58 +0200

    Update RegisterController.php

--- app/Http/Controllers/Frontend/RegisterController.php
+++ app/Http/Controllers/Frontend/RegisterController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Models\Guardian;
+use Illuminate\Http\Request;
+use Str;
 
 class RegisterController extends Controller
 {
@@ -16,9 +19,22 @@ class RegisterController extends Controller
         return view('frontend.register.register');
     }
 
-    public function registerParent()
+    public function registerParent(Request $request)
     {
-        
+        $parent_first_name = $request->input('parent_first_name');
+        $parent_last_name = $request->input('parent_last_name');
+        $parent_mail = $request->input('parent_mail');
+        $parent_phone = $request->input('parent_phone');
+
+        $reference = Str::uuid();
+
+        Guardian::create([
+            'first_name' => $parent_first_name,
+            'last_name' => $parent_last_name,
+            'phone' => $parent_phone,
+            'mail' => $parent_mail,
+            'reference' => $reference,
+        ]);
     }
 
     public function completed()
