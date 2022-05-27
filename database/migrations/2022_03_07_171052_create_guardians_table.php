5e1c4ffb75617d4b56f5e873a6d8a789d00065cb
Caspar Brenneisen <github@brenneisen.me>
2022-05-27 17:41:37 +0200

    Update 2022_03_07_171052_create_guardians_table.php

--- database/migrations/2022_03_07_171052_create_guardians_table.php
+++ database/migrations/2022_03_07_171052_create_guardians_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('parents', function (Blueprint $table) {
+        Schema::create('guardians', function (Blueprint $table) {
             $table->id();
 
             $table->string('first_name');
