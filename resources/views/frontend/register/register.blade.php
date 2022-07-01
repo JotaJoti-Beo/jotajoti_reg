5e1434b2219861728c50d91818a1982a57c7fec9
Caspar Brenneisen <github@brenneisen.me>
2022-07-01 16:07:53 +0200

    Update register.blade.php

--- resources/views/frontend/register/register.blade.php
+++ resources/views/frontend/register/register.blade.php
@@ -12,77 +12,84 @@
 
         <br />
 
+        <p class="text-center">
+            Bitte gib im folgenden die Information für einen Erziehungsberechtigten ein. <br />
+            Die Daten werden während dem Lager als Notfallkontakt verwendet. Die Person muss entsprechend während dem Lager erreichbar sein.
+        </p>
+
+        <br />
+
         <div class="col-10 offset-1">
             <form method="POST" action="{{ url('/register/guardian') }}" accept-charset="UTF-8" role="form" class="needs-validation form-horizontal">
                 @csrf
 
-                <div class="row has-feedback {{ $errors->has('parent_first_name') ? ' has-error ' : '' }}">
-                    <label for="parent_first_name" class="col-md-3 form-label">Vorname Erziehungsberechtigte</label>
+                <div class="row has-feedback {{ $errors->has('first_name') ? ' has-error ' : '' }}">
+                    <label for="first_name" class="col-md-3 form-label">Vorname Erziehungsberechtigte</label>
                     <div class="col-md-9">
                         <div class="input-group mb-3">
-                            <input id="parent_first_name" class="form-control" placeholder="Vorname Erziehungsberechtigte" name="parent_first_name" type="text" />
+                            <input id="first_name" class="form-control" placeholder="Vorname Erziehungsberechtigte" name="first_name" type="text" required />
 
-                            <label class="input-group-text" for="parent_first_name">
+                            <label class="input-group-text" for="first_name">
                                 <i class="fa fa-user" aria-hidden="true"></i>
                             </label>
                         </div>
-                        @if ($errors->has('parent_first_name'))
+                        @if ($errors->has('first_name'))
                             <span class="help-block">
-                                <strong>{{ $errors->first('parent_first_name') }}</strong>
+                                <strong>{{ $errors->first('first_name') }}</strong>
                             </span>
                         @endif
                     </div>
                 </div>
 
-                <div class="row has-feedback {{ $errors->has('parent_last_name') ? ' has-error ' : '' }}">
-                    <label for="parent_last_name" class="col-md-3 form-label">Nachname Erziehungsberechtigte</label>
+                <div class="row has-feedback {{ $errors->has('last_name') ? ' has-error ' : '' }}">
+                    <label for="last_name" class="col-md-3 form-label">Nachname Erziehungsberechtigte</label>
                     <div class="col-md-9">
                         <div class="input-group mb-3">
-                            <input id="parent_last_name" class="form-control" placeholder="Nachname Erziehungsberechtigte" name="parent_last_name" type="text" />
+                            <input id="last_name" class="form-control" placeholder="Nachname Erziehungsberechtigte" name="last_name" type="text" required />
 
-                            <label class="input-group-text" for="parent_last_name">
+                            <label class="input-group-text" for="last_name">
                                 <i class="fa fa-user" aria-hidden="true"></i>
                             </label>
                         </div>
-                        @if ($errors->has('parent_last_name'))
+                        @if ($errors->has('last_name'))
                             <span class="help-block">
-                                <strong>{{ $errors->first('parent_last_name') }}</strong>
+                                <strong>{{ $errors->first('last_name') }}</strong>
                             </span>
                         @endif
                     </div>
                 </div>
 
-                <div class="row has-feedback {{ $errors->has('parent_mail') ? ' has-error ' : '' }}">
-                    <label for="parent_mail" class="col-md-3 form-label">E-Mail Erziehungsberechtigte</label>
+                <div class="row has-feedback {{ $errors->has('mail') ? ' has-error ' : '' }}">
+                    <label for="mail" class="col-md-3 form-label">E-Mail Erziehungsberechtigte</label>
                     <div class="col-md-9">
                         <div class="input-group mb-3">
-                            <input id="parent_mail" class="form-control" placeholder="E-Mail Erziehungsberechtigte" name="parent_mail" type="email" />
+                            <input id="mail" class="form-control" placeholder="E-Mail Erziehungsberechtigte" name="mail" type="email" required />
 
-                            <label class="input-group-text" for="parent_mail">
+                            <label class="input-group-text" for="mail">
                                 <i class="fa fa-envelope" aria-hidden="true"></i>
                             </label>
                         </div>
-                        @if ($errors->has('parent_mail'))
+                        @if ($errors->has('mail'))
                             <span class="help-block">
-                                <strong>{{ $errors->first('parent_mail') }}</strong>
+                                <strong>{{ $errors->first('mail') }}</strong>
                             </span>
                         @endif
                     </div>
                 </div>
 
-                <div class="row has-feedback {{ $errors->has('parent_phone') ? ' has-error ' : '' }}">
-                    <label for="parent_phone" class="col-md-3 form-label">Telefon Erziehungsberechtigte</label>
+                <div class="row has-feedback {{ $errors->has('phone') ? ' has-error ' : '' }}">
+                    <label for="phone" class="col-md-3 form-label">Telefon Erziehungsberechtigte</label>
                     <div class="col-md-9">
                         <div class="input-group mb-3">
-                            <input id="parent_phone" class="form-control" placeholder="Telefon Erziehungsberechtigte" name="parent_phone" type="tel" />
+                            <input id="phone" class="form-control" placeholder="Telefon Erziehungsberechtigte" name="phone" type="tel" required />
 
-                            <label class="input-group-text" for="parent_phone">
+                            <label class="input-group-text" for="phone">
                                 <i class="fa fa-phone" aria-hidden="true"></i>
                             </label>
                         </div>
-                        @if ($errors->has('parent_phone'))
+                        @if ($errors->has('phone'))
                             <span class="help-block">
-                                <strong>{{ $errors->first('parent_phone') }}</strong>
+                                <strong>{{ $errors->first('phone') }}</strong>
                             </span>
                         @endif
                     </div>
