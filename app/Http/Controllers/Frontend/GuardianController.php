11af602608765c4883aaf9a7c646481ca8281711
Caspar Brenneisen <github@brenneisen.me>
2022-08-09 18:38:32 +0200

    Update GuardianController.php

--- app/Http/Controllers/Frontend/GuardianController.php
+++ app/Http/Controllers/Frontend/GuardianController.php
@@ -13,13 +13,20 @@ class GuardianController
         return view('frontend.register.guardian.show', ['guardian' => $guardian]);
     }
 
-    public function edit()
+    public function edit($guardid)
     {
+        $guardian = Guardian::find($guardid);
 
+        return view('frontend.register.guardian.edit', ['guardian' => $guardian]);
     }
 
-    public function update()
+    public function update($guardid)
     {
+        $guardian = Guardian::update([$guardid],
+            [
+                ''
+            ]);
 
+        return redirect()->back()->with('message', 'LUL');
     }
 }
