14044e040c790d8dde3bccb841c8c288302aa9a9
Caspar Brenneisen <github@brenneisen.me>
2022-08-22 14:03:08 +0200

    Update Helpers.php

--- app/Helpers/Helpers.php
+++ app/Helpers/Helpers.php
@@ -6,10 +6,10 @@ use Illuminate\Support\Carbon;
 
 class Helpers
 {
-  public static function formatDateFromDB($date)
-  {
-      $carbon_birthday = Carbon::createFromFormat('Y-m-d', $date);
+    public static function formatDateFromDB($date)
+    {
+        $carbon_birthday = Carbon::createFromFormat('Y-m-d', $date);
 
-      return $carbon_birthday->format('d.m.Y');
-  }
+        return $carbon_birthday->format('d.m.Y');
+    }
 }
